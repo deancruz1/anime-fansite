b462163c6473a6b18e4c32666b68935c52a55f3c
Dean Cruz <cruzdean1997@gmail.com>
2023-10-30 18:29:30 +0800

    Add content to characters page

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
-function changeBg(vid, title) {
+function changeBg(vid, img, title) {
   const video = document.querySelector('.bg-video');
   video.src = `./assets/characters-page/bg/${vid}`;
   video.load();
 
-  const contents = document.querySelectorAll('.content');
+  const crest = document.querySelector('.char-crest');
+  crest.src = './assets/characters-page/crest/' + img;
 
+  const contents = document.querySelectorAll('.content');
   contents.forEach(content => {
     content.classList.remove('active');
     if (content.classList.contains(title)) {
