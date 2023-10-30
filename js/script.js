34ddf99b90da82fdd7d609d7e966948b7c8f05e2
Dean Cruz <cruzdean1997@gmail.com>
2023-10-30 16:57:22 +0800

    Add characters page

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function changeBg(vid, title) {
+  const video = document.querySelector('.bg-video');
+  video.src = `../assets/characters-page/bg/${vid}`;
+  video.load();
+
+  const contents = document.querySelectorAll('.content');
+
+  contents.forEach(content => {
+    content.classList.remove('active');
+    if (content.classList.contains(title)) {
+      content.classList.add('active');
+    }
+  });
+}
+
+$(document).ready(function () {
+  $('.carousel').carousel();
+});
